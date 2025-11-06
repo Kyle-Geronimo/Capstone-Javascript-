9c56fcf039925b1ca8459a3463e234a0a384d40a
Kyle-Geronimo <KyleGeronimo360@gmail.com>
2025-11-06 16:17:42 +0800

    Update node.js

--- javascript_website/js/node.js
+++ javascript_website/js/node.js
@@ -3,6 +3,5 @@ var admin = require("firebase-admin");
 var serviceAccount = require("path/to/serviceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://capstone-a773d-default-rtdb.asia-southeast1.firebasedatabase.app"
+  credential: admin.credential.cert(serviceAccount)
 });
