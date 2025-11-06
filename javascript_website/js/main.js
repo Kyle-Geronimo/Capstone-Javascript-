955acdae55da9cf4c43a610d0dc3c514c8df8bea
Kyle-Geronimo <KyleGeronimo360@gmail.com>
2025-11-06 16:17:05 +0800

    Update main.js

--- javascript_website/js/main.js
+++ javascript_website/js/main.js
@@ -1,7 +1,6 @@
 import { auth } from './firebase-config.js';
 import { loadProfile } from './profile.js';
 import { loadChatbotData } from './chatbot.js';
-import { loadModuleStatus } from './modules.js';
 
 auth.onAuthStateChanged(user => {
   const logoutBtn = document.getElementById('logout-btn');
@@ -9,7 +8,6 @@ auth.onAuthStateChanged(user => {
     logoutBtn.style.display = '';
     loadProfile(user.uid);
     loadChatbotData();
-    loadModuleStatus();
   } else {
     logoutBtn.style.display = 'none';
   }
